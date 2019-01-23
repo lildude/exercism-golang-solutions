4935fd0eb3874619e70609f455516b734fb349f1
Colin Seymour <lildood@gmail.com>
2019-01-23 17:56:19 +0000

    Add hello-world solution

---
+++ hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting implements a single function that returns "Hello, World!"
+package greeting
+
+// HelloWorld returns a string "Hello, World!"
+func HelloWorld() string {
+	return "Hello, World!"
+}
