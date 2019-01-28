73d405793b65f3c00c6df876371612f68e873407
Colin Seymour <lildood@gmail.com>
2019-01-28 15:42:13 +0000

    Add initial scrabble-score solution

---
+++ scrabble-score/scrabble_score.go
@@ -0,0 +1,30 @@
+// Package scrabble provides a method to calculate scrabble scores.
+package scrabble
+
+import (
+	"strings"
+)
+
+// Score returns the Scrabble score for a word passed to it.
+func Score(word string) int {
+	score := 0
+	for _, letter := range strings.ToLower(word) {
+		switch string(letter) {
+		case "a", "e", "i", "o", "u", "l", "n", "r", "s", "t":
+			score++
+		case "d", "g":
+			score += 2
+		case "b", "c", "m", "p":
+			score += 3
+		case "f", "h", "v", "w", "y":
+			score += 4
+		case "k":
+			score += 5
+		case "j", "x":
+			score += 8
+		case "q", "z":
+			score += 10
+		}
+	}
+	return score
+}
